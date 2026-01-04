cdd22a2372fbb57a0259e7aea1050ed8f27e51d4
pollob debnath <pullobdebnath@gmail.com>
2026-01-05 01:31:19 +0600

    adding navbar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,10 +4,28 @@ import "../../App.css";
 
 const Navigation = () => {
   return (
-    <div className="navigation ">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/register">Register</NavLink>
+    <div>
+      <div className="navbar bg-gray-600">
+        <div className="navbar-start">
+          <a className="btn btn-ghost text-xl ml-5 ">Auth</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className=" text-xl">
+            <NavLink to="/">Home</NavLink>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div className="login-register flex gap-5 mr-10 text-xl">
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/register">Register</NavLink>
+          </div>
+        </div>
+      </div>
+      {/* <div className="navigation ">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/register">Register</NavLink>
+      </div> */}
     </div>
   );
 };
